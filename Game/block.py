47fd9cbb3f48dfd0f77f1e037489b31ce707e20b
THaraSin <92517144+THaraSin@users.noreply.github.com>
2021-11-23 14:02:53 +0700

    improvement

---
+++ Game/block.py
@@ -0,0 +1,11 @@
+form ursina import *
+
+class NormalBlok(Entity):
+    def __init__(self, position = (0, 0, 0), rotation = (0, 0, 0)):
+        super().__init__(
+            model = "cube",
+            collider = "box",
+            scale = (3, 0,8, 3),
+            color = "#F26A1B",
+            texture = "white_cube"
+        )
